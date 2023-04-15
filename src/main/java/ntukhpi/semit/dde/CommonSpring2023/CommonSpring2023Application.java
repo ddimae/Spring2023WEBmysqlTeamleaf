d268a8e9e97585874a12309279d018ea50fae78f
ddimae72 <ddimae72@gmail.com>
2023-04-15 09:59:43 +0300

    emmployee console. This version create table 'employee' base on Entity Employee. In CommonSpring2023Application.java was presented @Bean CommmandLineRunner in which you can run cod which save 10 records in table and read it from table. In future you can add some code here.

--- src/main/java/ntukhpi/semit/dde/CommonSpring2023/CommonSpring2023Application.java
+++ src/main/java/ntukhpi/semit/dde/CommonSpring2023/CommonSpring2023Application.java
@@ -1,7 +1,16 @@
 package ntukhpi.semit.dde.CommonSpring2023;
 
+import ntukhpi.semit.dde.CommonSpring2023.entity.Employee;
+import ntukhpi.semit.dde.CommonSpring2023.repository.EmployeeRepository;
+import ntukhpi.semit.dde.CommonSpring2023.service.EmployeeService;
+import ntukhpi.semit.dde.CommonSpring2023.service.impl.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CommonSpring2023Application {
@@ -9,6 +18,50 @@ public class CommonSpring2023Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(CommonSpring2023Application.class, args);
+	}
+
+	@Autowired
+	EmployeeService employeeService;
+	@Autowired
+	EmployeeRepository employeeRepository;
+	@Bean
+	public CommandLineRunner CommandLineRunnerBean() {
+
+		return (args) -> {
+			System.out.println("In CommandLineRunnerImpl ");
+			TestEmployee();
+
+		};
+	}
+
+	private void TestEmployee() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("Test Employee Spring ");
+		employeeService = new EmployeeServiceImpl(employeeRepository);
+		String[] names = new String[]{"Zhuk", "Kot", "Gusin", "Zhatova", "Shatova", "Svatok", "Katz", "Kotov", "Lomov", "Popova"};
+		employeeService.saveEmployee(new Employee(0l, names[0], true, 35, 120000.0));
+		//session.save(new Employee(0l, "Kot", true, 51, 2500000.0));
+		employeeService.saveEmployee(new Employee(0l, names[1], true, 55, 55000.0));
+		//session.save(new Employee(0l, "Gus'", true, 60, 99000.0));
+		employeeService.saveEmployee(new Employee(0l, names[2], true, 34, 99000.0));
+		employeeService.saveEmployee(new Employee(0l, names[3], false, 28, 99000.0));
+		employeeService.saveEmployee(new Employee(0l, names[4], false, 29, 99000.0));
+		employeeService.saveEmployee(new Employee(0l, names[5], true, 32, 70000.0));
+		employeeService.saveEmployee(new Employee(0l, names[6], true, 37, 75000.0));
+		employeeService.saveEmployee(new Employee(0l, names[7], true, 39, 55000.0));
+		employeeService.saveEmployee(new Employee(0l, names[8], true, 33, 90000.0));
+		employeeService.saveEmployee(new Employee(0l, names[9], false, 32, 50000.0));
+
+		System.out.println("\nEmploee list in spvr.employees");
+		List<Employee> employee =  employeeService.getAllEmployees();
+		employee.stream().forEach(System.out::println);
+
+
+		System.out.println("END SHOW employees WITH Spring");
 	}
 
 }
--- src/main/java/ntukhpi/semit/dde/CommonSpring2023/entity/Employee.java
+++ src/main/java/ntukhpi/semit/dde/CommonSpring2023/entity/Employee.java
@@ -1,9 +1,9 @@
 package ntukhpi.semit.dde.CommonSpring2023.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.internal.build.AllowPrintStacktrace;
 import org.springframework.stereotype.Service;
 
 @Entity
@@ -12,12 +12,31 @@ import org.springframework.stereotype.Service;
 @NoArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@ToString
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @Column(name = "id")
+    private Long id;  //Long!!! not long, not int
+    @Column(name = "name", nullable=false, unique=true, length = 50)
     private String name;
+
+    @Column(name = "pol", nullable=false)
+    @ColumnDefault(value = "true")
+    private boolean pol;
+    //@Column(name = "age", columnDefinition = "value>=18 AND value<=70 defauit 18",nullable=false)
+    //@ColumnDefault(value = "18")
+
+    @Column(name="age", columnDefinition ="int not null DEFAULT 18, CONSTRAINT range_age CHECK (age>=18 AND age<=60)")
+    //@Column(name="age", nullable = false)
+    //@Check(constraints = "age>=18 AND age<=60")
+    //@ColumnDefault(value = "18")
+    private int age;
+
+    @Column(name = "salary", columnDefinition ="DOUBlE(10,2) not null DEFAULT 6500.00, CONSTRAINT int_salary CHECK (salary>=6500)")
+    //@ColumnDefault(value = "6500")
+    private Double salary;
 
 }
