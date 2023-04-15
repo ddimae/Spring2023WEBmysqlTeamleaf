b37022e03ce13079262217e2fa9f91d782985050
ddimae72 <ddimae72@gmail.com>
2023-04-15 10:26:45 +0300

    employee wep-app. Before run you can comment row in CommonSpring2023Application.java#CommmandLineRunner and change property spring.jpa.hibernate.ddl-auto to "update". http://localhost:8080/employees - it is link for show Employee list.

--- src/main/java/ntukhpi/semit/dde/CommonSpring2023/controller/EmployeeController.java
+++ src/main/java/ntukhpi/semit/dde/CommonSpring2023/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package ntukhpi.semit.dde.CommonSpring2023.controller;
 
+import ntukhpi.semit.dde.CommonSpring2023.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,24 @@ public class EmployeeController {
 
     // handler method to handle list students and return mode and view
     @GetMapping("/")
-    public String helloPage(Model model) {
+    public String helloPage() {
 
         return "hellopage";
     }
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        super();
+        this.employeeService = employeeService;
+    }
+
+    // handler method to handle list students and return mode and view
+    @GetMapping("/employees")
+    public String listEmployees(Model model) {
+        model.addAttribute("employees", employeeService.getAllEmployees());
+        return "employees";
+    }
+
+
 }
